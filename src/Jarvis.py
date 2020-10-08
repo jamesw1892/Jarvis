bf73d1c51e35b847909fc6c59d2969e5ee2c48ab
James Wilson <gingersemaj1892@gmail.com>
2020-10-08 20:28:24 +0100

    Need to fix kivy logging interfering with ours

--- src/Jarvis.py
+++ src/Jarvis.py
@@ -1,7 +1,6 @@
 from Validation import true_false
-
 from kivy.utils import platform # str equal to one of ‘win’, ‘linux’, ‘android’, ‘macosx’, ‘ios’ or ‘unknown’
-
+import IO
 
 if platform == "win":
     from OSWindows import main
